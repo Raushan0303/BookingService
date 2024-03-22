2208a92dd9f4033242b5669600ef74421306eef9
Raushan0303 <110516637+Raushan0303@users.noreply.github.com>
2024-03-22 15:02:34 +0530

    hh

--- src/controllers/Booking-controller.js
+++ src/controllers/Booking-controller.js
@@ -2,27 +2,44 @@ const BookingService = require('../services/Booking-service');
 const {StatusCodes} = require('http-status-codes')
 
 
+const { createChannel, publishMessage } = require('../utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('../config/serverconfig');
+
 const bookingService = new BookingService();
 
-const create = async(req,res)=>{
-    try {
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json({
-            message: 'Successfully completed Booking',
-            success: true,
-            err: {},
-            data: response
-        })
-    } catch (error) {
-        return res.status(error.statusCode).json({
-            message: error.message,
-            success: false,
-            err: error.explanation,
-            data: {}
-        })
+class BookingController {
+
+    constructor() {
+    }
+
+    async sendMessageToQueue(req, res){
+        const channel = await createChannel();
+        const data = {message: 'Success'};
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        return res.status(200).json({
+            message: 'Succesfully published the event'
+        });
+    }
+
+    async create (req, res) {
+        try {
+            const response = await bookingService.createBooking(req.body);
+            console.log("FROM BOOKING CONTROLLER", response);
+            return res.status(StatusCodes.OK).json({
+                message: 'Successfully completed booking',
+                success: true,
+                err: {},
+                data: response
+            })
+        } catch (error) {
+            return res.status(error.statusCode).json({
+                message: error.message,
+                success: false,
+                err: error.explanation,
+                data: {}
+            });
+        }
     }
 }
 
-module.exports = {
-    create
-}
+module.exports = BookingController
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -13,14 +13,35 @@ class BookingService{
     async createBooking(data){
         try {
             const flightId = data.flightId;
-            const getFlighRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
-            const flight = await axios.get(getFlighRequestUrl);
-            console.log(flight.data);
-            return flight.data;
-        } catch (error) {
+            const getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
+            const response = await axios.get(getFlightRequestURL);
+            const flightData = response.data.data;
+            let priceOfTheFlight = flightData.price;
+            if(data.noOfSeats > flightData.totalSeats) {
+                throw new ServiceError('Something went wrong in the booking process', 'Insufficient seats in the flight');
+            }
+            const totalCost = priceOfTheFlight * data.noOfSeats;
+            const bookingPayload = {...data, totalCost};
+            const booking = await this.bookingRepository.create(bookingPayload);
+            const updateFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`;
+            console.log(updateFlightRequestURL);
+            await axios.patch(updateFlightRequestURL, {totalSeats: flightData.totalSeats - booking.noOfSeats});
+            const finalBooking = await this.bookingRepository.update(booking.id, {status: "Booked"});
+            return finalBooking;
+        } catch (error) { 
+            console.log(error);
+            if(error.name == 'RepositoryError' || error.name == 'ValidationError') {
+                throw error;
+            }
             throw new ServiceError();
         }
-    }
+    } 
 }
 
 module.exports = BookingService;
+
+//homework
+
+// 1. implement update booking api
+// 2. read about winston
+// 3. complete error handling in other service
